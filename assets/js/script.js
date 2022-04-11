de29255a2325fff2ab18e582c488d15883e48328
marosan28 <danijela.marosan36@gmail.com>
2022-04-11 07:37:00 +0000

    Add next buttons and declare in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,4 @@
 // Button declarations
 const startBtn = document.getElementById('startBtn');
+const nextUsername = document.getElementById('nextUsername');
+const nextQuestion = document.getElementById('nextQuestion');
