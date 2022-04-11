e3b4efe15aff81ecf5eaf25918906433c0eb277d
marosan28 <danijela.marosan36@gmail.com>
2022-04-11 08:23:50 +0000

    Add questions and answers to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,81 @@ const usernameInput = document.getElementById('usernameInput');
 const usernameScore = document.getElementById('usernameScore');
 const questionAnswers = document.getElementById('questionAnswers');
 const endScreen = document.getElementById('endScreen');
+// Misc
+let username;
+let score = 0;
+let start = true;
+let id = 0;
+let inputValue;
+// Questions object
+const questions = [
+    {
+      question: 'Question one',
+      answers: [
+        {
+        text: 'Answer 1', isCorrect: true,
+        },
+        {
+        text: 'Answer 2', isCorrect: false,
+        },
+        {
+        text: 'Answer 3', isCorrect: false,
+        },
+        {
+        text: 'Answer 4', isCorrect: false,
+        },
+      ]
+    },
+    {
+      question: 'Question two',
+      answers: [
+        {
+        text: 'Answer 1', isCorrect: false,
+        },
+        {
+        text: 'Answer 2', isCorrect: true,
+        },
+        {
+        text: 'Answer 3', isCorrect: false,
+        },
+        {
+        text: 'Answer 4', isCorrect: false,
+        },
+      ]
+    },
+    {
+      question: 'Question three',
+      answers: [
+        {
+        text: 'Answer 1', isCorrect: false,
+        },
+        {
+        text: 'Answer 2', isCorrect: false,
+        },
+        {
+        text: 'Answer 3', isCorrect: true,
+        },
+        {
+        text: 'Answer 4', isCorrect: false,
+        },
+      ]
+    },
+    {
+      question: 'Question four',
+      answers: [
+        {
+        text: 'Answer 1', isCorrect: false,
+        },
+        {
+        text: 'Answer 2', isCorrect: false,
+        },
+        {
+        text: 'Answer 3', isCorrect: false,
+        },
+        {
+        text: 'Answer 4', isCorrect: true,
+        },
+      ]
+    },
+  ]
+  
