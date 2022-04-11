f0279558b226b5d4aecc7f80fe6a743f91df0abe
marosan28 <danijela.marosan36@gmail.com>
2022-04-11 07:41:44 +0000

    Declare text elements in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,9 @@ const startBtn = document.getElementById('startBtn');
 const nextUsername = document.getElementById('nextUsername');
 const nextQuestion = document.getElementById('nextQuestion');
 const resetBtn = document.getElementById('resetBtn');
-
+// Text elements
+const startText = document.getElementById('startText');
+const usernameDisplay = document.getElementById('usernameDisplay');
+const scoreDisplay = document.getElementById('scoreDisplay');
+const endUsername = document.getElementById('usernameEnd');
+const endScore = document.getElementById('scoreEnd');
