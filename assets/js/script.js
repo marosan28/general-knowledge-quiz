fa6438c0ea25afc1f106bca91e01ee1302e6bcb7
marosan28 <danijela.marosan36@gmail.com>
2022-04-11 07:40:42 +0000

    Add end screen score and username

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,5 @@
 const startBtn = document.getElementById('startBtn');
 const nextUsername = document.getElementById('nextUsername');
 const nextQuestion = document.getElementById('nextQuestion');
+const resetBtn = document.getElementById('resetBtn');
+
